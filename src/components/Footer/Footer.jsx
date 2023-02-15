21cf5dd37a6bc80fe545417a98cdad6208082e5e
SiddharthNand <sidharthnand47@gmail.com>
2023-02-15 14:31:03 -0800

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,38 @@ import './Footer.css';
 export default function Footer () {
   return (
       <footer>
+        <ul class="wrapper">
+        <li class="icon facebook">
+                  <a href="https://www.yelp.com/biz/united-party-rentals-sacramento-3" target="_blank" rel="noreferrer">
+                  <span class="tooltip">Yelp</span>
+                  <span><img src="https://i.imgur.com/KfnvNoc.png" 
+                  alt='bbb'
+                  height="40px"
+                  />
+                  </span>
+                  </a>
+              </li>
+              <li class="icon twitter">
+                  <a href="https://goo.gl/maps/Z6bD4CAEzf29TQMt8" target="_blank" rel="noreferrer">
+                    <span class="tooltip">Google</span>
+                    <span><img src="https://i.imgur.com/m8cpY8h.png" 
+                  alt='bbb'
+                  height="40px"
+                  />
+                  </span>
+                  </a>
+              </li>
+              <li class="icon instagram">
+                  <a href="https://www.bbb.org/us/ca/sacramento/profile/party-supplies/united-party-rentals-1156-37000504" target="_blank" rel="noreferrer">
+                  <span class="tooltip">BBB</span>
+                  <span><img src="https://i.imgur.com/JGf1ptV.png" 
+                  alt='bbb'
+                  height="45px"
+                  />
+                  </span>
+                  </a>
+              </li>
+        </ul>
   <p>Copyright © 2022 United Party Rentals 
     <br></br>
     Designed by: <a href="https://www.linkedin.com/in/joshua-campbell-3608231a4/" target="_blank" rel="noreferrer">Joshua Campbell</a> &nbsp; Created by: <a href="https://sidharthnand.dev/" target="_blank" rel="noreferrer">Sidharth Nand </a>
