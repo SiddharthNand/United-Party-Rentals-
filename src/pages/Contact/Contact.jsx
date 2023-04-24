e847ce00fad529e2780a84a6edc59fbef0c0dfad
SiddharthNand <sidharthnand47@gmail.com>
2023-04-24 14:56:43 -0700

    contact page before API

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -59,6 +59,28 @@ function Contact() {
           </form>
         </form>
       </div>
+<br></br>
+      <div className="businfo">
+                <h1 className="title2">VISIT OUR SHOWROOM</h1>
+
+                <p className="title addy"><a href="https://goo.gl/maps/t9MAizKhZt6iyDXS9">7232 Stockton Blvd. Sacramento, CA 95823</a></p>
+                <p><a className="title addy" href="tel:+19164248300">(916)-424-8300</a></p>
+                <div className="title1 title">
+                    <p className="title addy"><strong className="seasonHead">Winter Hours (Jan-Mar): </strong>
+                        <br></br>Monday to Friday: 9:00 am- 4:00 pm
+                        <br></br> Saturday: 9:00 am - 2:00 pm
+                        <br></br>Sunday: CLOSED</p>
+
+                    <br></br>
+
+                    <p className="title addy"><strong className="seasonHead">Summer Hours (April - Dec):</strong>
+                        <br></br>Monday to Friday: 9:00 am- 5:00 pm
+                        <br></br>Saturday: 9:00 am- 3:00 pm
+                        <br></br>Sunday: CLOSED
+                    </p>
+                </div>
+                <a href="tel:+19164248300" className="contact">Contact Us</a>
+            </div>
 
     </>
   );
